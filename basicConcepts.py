967caa4cad39b454d275e98f77b565aadb1fa382
dkodotcom <kodiana@outlook.com>
2017-05-31 17:27:51 -0700

    python concepts

---
+++ basicConcepts.py
@@ -0,0 +1,70 @@
+x = 10
+
+z = "string"
+
+y = float(10.5)
+
+def name(Diana):
+    print("\nHi my name is {}".format(name))
+
+a = 5
+b = 10
+
+print(b-a)
+5
+
+print(a+b)
+15
+
+print(a*b)
+50
+
+print(b/a)
+2
+
+print(b%a)
+0
+
+# demonstrating while statements with += operator
+counter = 0
+while counter <= 5:
+    print(counter) 
+    counter += 1
+
+# demonstrating if, elif, else statements
+def statements():
+    if counter < 0:
+        print("no output")
+    elif counter <5:
+        print("limit")
+    else: 
+        print("no value")
+
+# demonstrating the not logical operator
+def hi():
+    if hi == hi:
+        print("sup")
+    elif hi != hi:
+        print("mmhmbye")
+
+# demonstrating the and operator
+if x<4 and x<7:
+    print("condition met")
+
+# lack of creativity to demonstrate for loops and printing list
+loops = ["for", "while"]
+for statements in loops:
+    print(statements)
+
+# tuple, iterate and print
+tuple = ('a', 'b', 'c', 'd', 'e')
+for x in tuple:
+    print(x)
+
+# function that returns a string variable and prints 
+def someInteger(my_tuple):
+    bad = my_tuple[1]+my_tuple[0]+my_tuple[3]
+    return bad 
+print(someInteger(tuple))
+
+
